d9756e635e888e515983b5055a30d02b53d45a5e
Paulo Ricardo <pauloricardosm@alu.ufc.br>
2024-09-02 09:45:36 -0300

    adicionei as funções que estavam em falta no arquivo script.

--- src/js/script.js
+++ src/js/script.js
@@ -1,102 +1,131 @@
- $(document).ready(function() {
-    $('input[type="radio"][name="mortgageType"]').change(function() {
-        $('input[type="radio"][name="mortgageType"]').each(function() {
-            if ($(this).is(':checked')) {
-                $(this).closest('.mortType').css('background-color', 'hsla(61, 70%, 52%, 0.2)');
-                $(this).closest('.mortType').css('border-color', 'hsl(61, 70%, 52%)')
-            } else {
-                $(this).closest('.mortType').css('background-color', '');
-                $(this).closest('.mortType').css('border-color', '');
-            }
-        });
-    });  
+$(document).ready(function () {
+  $('input[type="radio"][name="mortgageType"]').change(function () {
+    $('input[type="radio"][name="mortgageType"]').each(function () {
+      if ($(this).is(":checked")) {
+        $(this)
+          .closest(".mortType")
+          .css("background-color", "hsla(61, 70%, 52%, 0.2)");
+        $(this).closest(".mortType").css("border-color", "hsl(61, 70%, 52%)");
+      } else {
+        $(this).closest(".mortType").css("background-color", "");
+        $(this).closest(".mortType").css("border-color", "");
+      }
+    });
+  });
 
-    $('#btnClearAll').click(function() {
-        $('#completedResu').css('display', 'none');
-        $('#emptyResu').css('display', 'block');
+  $("#btnClearAll").click(function () {
+    $("#completedResu").css("display", "none");
+    $("#emptyResu").css("display", "block");
 
-        $('.mortType').css('background-color', '');
-        $('.mortType').css('border-color', '');
+    $(".mortType").css("background-color", "");
+    $(".mortType").css("border-color", "");
+  });
+
+  $("#btnSubmit").click(function (event) {
+    event.preventDefault();
+
+    let hasErros = false;
+    const fields = [
+      { name: "mortgageAmount", errorId: "#mortgageAmountError", symbol: "#leftSymbol" },
+      { name: "mortgageTerm", errorId: "#mortgageTermError", symbol: "#sYears" },
+      { name: "interestRate", errorId: "#interestRateError", symbol: "#sPercentage" },
+    ];
+
+    fields.forEach((campo) => {
+      if ($(`input[name="${campo.name}"]`).val().trim() === "") {
+        $(campo.errorId).css("display", "block");
+        $(campo.symbol).css('background-color', 'hsl(4, 69%, 50%)');
+        $(campo.symbol).css('color', 'white');
+        hasErros = true;
+      } else {
+        $(campo.errorId).css("display", "none");
+        $(campo.symbol).css('background-color', '');
+        $(campo.symbol).css('color', '');
+      }
     });
 
-    $("#btnSubmit").click(function(event) {
-        event.preventDefault();
+    if (!$('input[name="mortgageType"]:checked').length) {
+      $("#mortgageTypeError").css("display", "block");
+      hasErros = true;
+    } else {
+      $("#mortgageTypeError").css("display", "none");
+      hasErros = false;
+    }
 
-        let hasErros = false;
+    function repaymentType(mortgageAmount, mortgageTerm, interestRate) {
+      let monthlyInterestRate = interestRate / 100 / 12;
+      let numberPayments = mortgageTerm * 12;
+      let monthlyRepayment =
+        mortgageAmount *
+        ((monthlyInterestRate *
+          Math.pow(1 + monthlyInterestRate, numberPayments)) /
+          (Math.pow(1 + monthlyInterestRate, numberPayments) - 1));
+      let totalPayment = monthlyRepayment * numberPayments;
 
-        const fields = [ 
-            {name: 'mortgageAmount', errorId: '#mortgageAmountError'},
-            {name: 'mortgageTerm', errorId: '#mortgageTermError'},
-            {name: 'interestRate', errorId: '#interestRateError'}
-        ];
+      return { monthlyRepayment, totalPayment };
+    }
 
-        fields.forEach(campo => {
-            if($(`input[name="${campo.name}"]`).val().trim() === '') {
-                $(campo.errorId).css('display', 'block');
-                hasErros = true;
-            } else {
-                $(campo.errorId).css('display', 'none');
-            }
-        });
+    function interestOnlyType(mortgageAmount, mortgageTerm, interestRate) {
+      let monthlyRepayment = mortgageAmount * (interestRate / 100 / 12);
+      let totalPayment = monthlyRepayment * mortgageTerm * 12;
 
-        if(!$('input[name="mortgageType"]:checked').length) {
-            $('#mortgageTypeError').css('display', 'block');
-            hasErros = true;
-        } else {
-            $('#mortgageTypeError').css('display', 'none');
-            hasErros = false;
-        }
+      return { monthlyRepayment, totalPayment };
+    }
 
-        function repaymentType(mortgageAmount, mortgageTerm, interestRate) {
-            let monthlyInterestRate = (interestRate / 100) / 12;
-            let numberPayments = mortgageTerm * 12;
-            let monthlyRepayment = mortgageAmount * ((monthlyInterestRate * Math.pow((1 + monthlyInterestRate), numberPayments))/((Math.pow((1 + monthlyInterestRate), numberPayments)) - 1))
-            let totalPayment = monthlyRepayment * numberPayments; 
+    if (!hasErros) {
+      let mortgageAmount = parseFloat($("#imortgageAmount").val());
+      let mortgageTerm = parseInt($("#imortgageTerm").val());
+      let interestRate = parseFloat($("#iinterestRate").val());
+      let mortgageType = $('input[name="mortgageType"]:checked').val();
 
-            return { monthlyRepayment, totalPayment };
-        }
+      if (isNaN(mortgageAmount) || isNaN(mortgageTerm) || isNaN(interestRate)) {
+        alert("Invalid values");
+        return;
+      }
 
-        function interestOnlyType(mortgageAmount, mortgageTerm, interestRate) {
-            let monthlyRepayment = mortgageAmount * ((interestRate / 100) / 12);
-            let totalPayment = monthlyRepayment * mortgageTerm * 12;
+      if (mortgageType == "repayment") {
+        let resu = repaymentType(mortgageAmount, mortgageTerm, interestRate);
+        let completeMonthlyResu = resu.monthlyRepayment;
+        let completeTotalPayment = resu.totalPayment;
 
-            return { monthlyRepayment, totalPayment };
-        }
+        $("#monthlyResu").text(
+          `£${completeMonthlyResu.toLocaleString("en-GB", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}`
+        );
+        $("#repayResu").text(
+          `£${completeTotalPayment.toLocaleString("en-GB", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}`
+        );
+      } else {
+        let resu = interestOnlyType(mortgageAmount, mortgageTerm, interestRate);
+        let completeMonthlyResu = resu.monthlyRepayment;
+        let completeTotalPayment = resu.totalPayment;
 
-        if(!hasErros) {
-            let mortgageAmount = parseFloat($('#imortgageAmount').val());
-            let mortgageTerm = parseInt($('#imortgageTerm').val());
-            let interestRate = parseFloat($('#iinterestRate').val());
-            let mortgageType = $('input[name="mortgageType"]:checked').val();
+        $("#monthlyResu").text(
+          `£${completeMonthlyResu.toLocaleString("en-GB", {
+            minimumFractiondigits: 2,
+            maximumFractionDigits: 2,
+          })}`
+        );
+        $("#repayResu").text(
+          `£${completeTotalPayment.toLocaleString("en-GB", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}`
+        );
+      }
 
-            if (isNaN(mortgageAmount) || isNaN(mortgageTerm) || isNaN(interestRate)) {
-                alert("Invalid values");
-                return;
-            }
+      $("#emptyResu").css("display", "none");
+      $("#completedResu").css("display", "block");
 
-            if(mortgageType == "repayment") {
-                let resu = repaymentType(mortgageAmount, mortgageTerm, interestRate);
-                let completeMonthlyResu = resu.monthlyRepayment;
-                let completeTotalPayment = resu.totalPayment;
-
-                $('#monthlyResu').text(`£${completeMonthlyResu.toLocaleString('en-GB', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`);
-                $('#repayResu').text(`£${completeTotalPayment.toLocaleString('en-GB', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`);
-            } else {
-                let resu = interestOnlyType(mortgageAmount, mortgageTerm, interestRate);
-                let completeMonthlyResu = resu.monthlyRepayment;
-                let completeTotalPayment = resu.totalPayment;
-
-                $('#monthlyResu').text(`£${completeMonthlyResu.toLocaleString('en-GB', {minimumFractiondigits: 2, maximumFractionDigits: 2})}`);
-                $('#repayResu').text(`£${completeTotalPayment.toLocaleString('en-GB', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`);
-            }
-
-            $('#emptyResu').css('display', 'none');
-            $('#completedResu').css('display', 'block');
-
-            window.scrollTo({
-                top: document.body.scrollHeight,
-                behavior: "smooth",
-            });
-        }
-    });
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  });
 });
